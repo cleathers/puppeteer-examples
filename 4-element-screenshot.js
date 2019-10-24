9c7981fa6538cdf8bd24911be43aab28578ba751
Charlie Leathers <charles.leathers@disney.com>
2019-10-24 12:41:15 -0700

    4-element-screenshot example

---
+++ 4-element-screenshot.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+	const browser = await puppeteer.launch();
+
+	const page = await browser.newPage();
+	await page.goto('https://abc.com', {waitUntil: 'networkidle2'});
+
+  await page.screenshot({path: 'pageshot.png'});
+
+  const el = await page.$$('nav.navigation');
+  await el.screenshot({path: 'example.png'});
+
+
+
+
+
+	process.exit();
+})();
